55e215641934d9fd6accfc95679ca14e28c35f36
= <jishnusundar423@gmail.com>
2017-01-21 16:07:33 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//your server will live here
+let connect = require('connect');
+
+//create an instance of the connect server - app
+let app = connect();
+
+//create the port constant
+const port = 3000;
+
+//start listening
+app.listen(port);
+console.log(`Server started at http://localhotst:${port}`);
