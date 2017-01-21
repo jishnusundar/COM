37b81904b3e2ca909fc4f18655da6aa4fc7ea126
= <jishnusundar423@gmail.com>
2017-01-21 16:27:37 -0500

    Added rout

--- server.js
+++ server.js
@@ -11,4 +11,14 @@ const port = 3000;
 app.listen(port);
 console.log(`Server started at http://localhotst:${port}`);
 
+//Route for hello
+app.use('/hello', (req,res,next)=> {
+res.setHeader('Content-Type', 'text/plain');
+res.end("Hello world!");
+});
+
 //first route is '/'- root of my website
+app.use('/', (req,res,next)=> {
+res.setHeader('Content-Type', 'text/plain');
+res.end("Welcome!");
+});
